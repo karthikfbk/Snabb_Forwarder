7bdbf3c0c3cf32dcfca62002c19d7ccfcc6804af
root <root@narva>
2016-07-10 18:33:47 +0000

    Adding lwaft files

---
+++ lwaftr/link.h
@@ -0,0 +1,26 @@
+/* Use of this source code is governed by the Apache 2.0 license; see COPYING. */
+
+enum { LINK_RING_SIZE    = 512,
+       LINK_MAX_PACKETS  = LINK_RING_SIZE - 1
+};
+
+struct link {
+  // this is a circular ring buffer, as described at:
+  //   http://en.wikipedia.org/wiki/Circular_buffer
+  struct packet *packets[LINK_RING_SIZE];
+  struct {
+    struct counter *txbytes, *rxbytes, *txpackets, *rxpackets, *txdrop;
+  } stats;
+  // Two cursors:
+  //   read:  the next element to be read
+  //   write: the next element to be written
+  int read, write;
+  // Index (into the Lua app.active_apps array) of the app that
+  // receives from this link.
+  int receiving_app;
+  // True when there are new packets to process.
+  // Set when a new packet is added to the ring and cleared after
+  // 'receiving_app' runs.
+  bool has_new_data;
+};
+
